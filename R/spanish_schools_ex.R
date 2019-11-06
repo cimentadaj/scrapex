8697f6603d37d52411863268ba2ad7b7e1eeab4f
Jorge Cimentada <cimentadaj@gmail.com>
2019-11-06 11:26:37 +0100

    Adds Spanish schools example

---
+++ R/spanish_schools_ex.R
@@ -0,0 +1,68 @@
+##' This example hosts data from the website https://www.buscocolegio.com/
+##' which tries to aggregate data on all schools in Spain. To build reliable
+##' and self-standing scraping examples, this package downloaded data on 27
+##' randomly sampled schools from this website. This function returns the full
+##' HTML links to the local website that can be used as exercises for scraping
+##' some data.
+##'
+##' All images should work well out of the box except the Google maps on the
+##' right, which is expected. Please note that these websites are not meant
+##' to be used to reliably explore their hyperlink or other features, as
+##' these will change inevitably when the website changes some of it's
+##' structures.
+##' 
+##' @title Return the paths to the complete HTML website of a sample of Spanish Schools
+##' @return A character vector with 27 local HTML links
+##' @author Jorge Cimentada
+##'
+##' @examples
+##'
+##' library(xml2)
+##'
+##' one_link <- spanish_schools_ex()[5]
+##'
+##' read_html(one_link)
+##'
+##' \dontrun{
+##' # If you want to look at the website in your browser
+##' # just wrap it in prep_browser and pass it to browseURL
+##' browseURL(prep_browser(one_link))
+##' }
+##'
+spanish_schools_ex <- function() {
+  pkg_dir <- system.file(package = "scrapex")
+  zip_data <- file.path(pkg_dir, "extdata")
+
+  # Unzip it after installation
+  unzip(file.path(zip_data, "spanish_schools_ex.zip"),
+        exdir = zip_data)
+  
+  html_files <-
+    list.files(file.path(zip_data, "spanish_schools_ex"),
+               pattern = "detalles-colegio\\.action.+\\.html$",
+               recursive = TRUE,
+               full.names = TRUE)
+
+  html_files
+}
+
+##' @title Prepare local HTML links to open with your browser
+##' @param x a character vector of HTML files
+##' @return the same character vector but with "file://" appended to it
+##' @author Jorge Cimentada
+##' @examples
+##'
+##' library(xml2)
+##'
+##' one_link <- spanish_schools_ex()[5]
+##' prep_browser(one_link)
+##'
+##' \dontrun{
+##' # You can open it with your browser with browseURL
+##' browseURL(prep_browser(one_link))
+##' }
+##' 
+prep_browser <- function(x) {
+  ready_html <- paste0("file://", x)
+  ready_html
+}
