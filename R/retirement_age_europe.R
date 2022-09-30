7cb4fa922388cae63a7d9fddfb529ec7d1ec0216
Jorge Cimentada <cimentadaj@gmail.com>
2022-09-30 18:06:12 +0200

    Adds age retirement example

---
+++ R/retirement_age_europe.R
@@ -0,0 +1,43 @@
+##' This example hosts data from the website https://en.wikipedia.org/wiki/Retirement_in_Europe
+##' which documents and collects data on the age of retirement for all of Europe.
+##'
+##' This function returns the full HTML links to the local HTML files that can be used as exercises for scraping the data as if this was the online hosted version.
+##'
+##' Please note that this website are not meant
+##' to be used to reliably explore their hyperlinks or other features, as
+##' these will change inevitably when the website changes some of it's
+##' structures. This is because some of the links still point to the
+##' real website or other platforms.
+##'
+##' @title Return the paths to the complete HTML website of ages at retirement in Europe
+##' @return A character vector with with 1 local HTML link
+##' @author Jorge Cimentada
+##' @export
+##'
+##' @examples
+##'
+##' library(xml2)
+##' library(scrapex)
+##'
+##' one_link <- retirement_age_europe_ex()
+##'
+##' read_html(one_link)
+##'
+##' \dontrun{
+##' # If you want to look at the website in your browser
+##' # just wrap it in prep_browser and pass it to browseURL
+##' browseURL(prep_browser(one_link))
+##' }
+##'
+retirement_age_europe_ex <- function() {
+  pkg_dir <- system.file(package = "scrapex")
+  dir_data <- file.path(pkg_dir, "extdata", "retirement_age_europe/")
+
+  html_files <-
+    list.files(dir_data,
+               pattern = "Retirement_.+\\.html$",
+               recursive = TRUE,
+               full.names = TRUE)
+
+  html_files
+}
