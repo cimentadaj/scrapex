213b479fd6942937866ef7a693cd35a47b47e2f4
Jorge Cimentada <cimentadaj@gmail.com>
2022-11-20 21:09:51 +0100

    Adds checks for correct region/sex parameters

--- R/api_coveragedb.R
+++ R/api_coveragedb.R
@@ -2,11 +2,11 @@
 ##'
 ##' COVerAGE-DB is an open-access database that includes cumulative counts of confirmed COVID-19 cases, deaths, tests, and vaccines by age and sex. The main goal of COVerAGE-DB is to provide a centralized, standardized, age-harmonized, and fully reproducible database of COVID-19 data. For more information, visit
 ##'
-##' This API wraps a custom download performed locally of the database and filters only 'California', 'Utah' and 'New York' for both sexes (`m` and `f` in the database).
+##' This API wraps a custom download performed locally of the database and filters only 'California', 'Utah' and 'New York State' for both sexes (`m` and `f` in the database).
 ##'
 ##' The API has two parameters which can be filtered:
 ##'
-##' * region: 'California', 'Utah' and 'New York'
+##' * region: 'California', 'Utah' and 'New York State'
 ##' * sex: `m` and `f`
 ##'
 ##'
@@ -31,6 +31,8 @@ filter_cases <- function(df) df[df$Measure == "Cases", ]
 filter_vaccine <- function(df) df[df$Measure %in% c("Vaccination1", "Vaccination2", "Vaccination3"), ]
 
 serve_api_coveragedb <- function(region, sex) {
+  match.arg(region, c("California", "Utah", "New York State"))
+  match.arg(sex, c("m", "f"))
   region_filter <- api_coveragedb_data$Region == region
   sex_filter <- api_coveragedb_data$Sex == sex
   tmp <- api_coveragedb_data[region_filter & sex_filter, ]
--- inst/plumber/api_coveragedb/plumber.R
+++ inst/plumber/api_coveragedb/plumber.R
@@ -1,14 +1,14 @@
 # api_coveragedb
 
-#* Access number of COVID cases for the American states California, Utah and New York.
-#* @param region The region to subset. Available only 'California', 'Utah' and 'New York'
+#* Access number of COVID cases for the American states California, Utah and New York State.
+#* @param region The region to subset. Available only 'California', 'Utah' and 'New York State'
 #* @param sex The sex to subset. Available only 'm' and 'f'
 #* @serialize json
 #* @get /api/v1/covid_cases
 scrapex:::serve_api_coveragedb_cases
 
-#* Access number of vaccinated COVID people for the American states California, Utah and New York.
-#* @param region The region to subset. Available only 'California', 'Utah' and 'New York'
+#* Access number of vaccinated COVID people for the American states California, Utah and New York State.
+#* @param region The region to subset. Available only 'California', 'Utah' and 'New York State'
 #* @param sex The sex to subset. Available only 'm' and 'f'
 #* @serialize json
 #* @get /api/v1/covid_vaccines
